be9b3fdd200ccc85c6f51ea15a26ad72fe2cd2b8
Sergi Ortiz Gómez <drualcman@msn.com>
2021-02-23 22:01:28 +0800

    Update jdDB to use promises

--- MyDbJS.js
+++ MyDbJS.js
@@ -11,51 +11,44 @@
         /**
          * Request all data from a table name. Return the data or JSON response
          * @param {string} table table name to request the data
-         * @param {function} callBack function to receive the result.
          */
-        Select: (table, callBack) => db.Select(table, callBack),
+        Select: (table) => db.Select(table),
         /**
          * Request data from a table name with the keyPath with a value. Return the data or JSON response
          * @param {string} table table name
          * @param {any} id id value
-         * @param {function} callBack function to receive the data
          */
-        SelectId: (table, id, callBack) => db.SelectId(table, id, callBack),
+        SelectId: (table, id) => db.SelectId(table, id),
         /**
          * Request data from a table name filtered by column name and value. Return the data or JSON response
          * @param {string} table table name
          * @param {string} column column name to filter
          * @param {any} value value for the filter
-         * @param {function} callBack function to receive the data
          */
-        SelectWhere: (table, column, value, callBack) => db.SelectWhere(table, column, value, callBack),
+        SelectWhere: (table, column, value) => db.SelectWhere(table, column, value),
         /**
          * Insert data into a table. Alway return a JSON response
          * @param {string} table table name
          * @param {JSON} data data with the model format to insert
-         * @param {function} callBack function to receive result
          */
-        Insert: (table, data, callBack) => db.Insert(table, data, callBack),
+        Insert: (table, data) => db.Insert(table, data),
         /**
          * Update data into the table. The data always must be content all the columns, if not the function retreive the actual data to keep always same data into a table. Alway return a JSON response
          * @param {string} table table name
          * @param {JSON} data data with the model format to update
-         * @param {function} callBack function to receive the result
          */
-        Update: (table, data, callBack) => Update(table, data, callBack),
+        Update: (table, data) => db.Update(table, data),
         /**
          * Dete one row from a table. Alway return a JSON response
          * @param {string} table table name
          * @param {any} id value from the keypath
-         * @param {function} callBack Function to receive the result
          */
-        Delete: (table, id, callBack) => Delete(table, id, callBack),
+        Delete: (table, id) => Delete(table, id),
         /**
          * Drop a table. Alway return a JSON response
          * @param {string} table table name
-         * @param {function} callBack function to receive the result
          */
-        Drop: (table, callBack) => Drop(table, callBack),
+        Drop: (table) => Drop(table),
         /**
          * To store files into a db for compatibility with most of the browsers
          * @param {bytes} buffer file buffer
